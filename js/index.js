f0d0d0802cfcce75d5d1661897b00300851acf94
rhose1234 <akorederhose@gmail.com>
2023-08-13 17:32:14 +0100

    yhhh

--- js/index.js
+++ js/index.js
@@ -1,19 +1,39 @@
 var navbar = document.querySelector(".navbar");
+var isScrolling = false;
 
-    // Add a scroll event listener to the window
-    window.addEventListener("scroll", function() {
-      var scroll = window.scrollY || window.pageYOffset;
+// Debounce function to limit the rate of execution
+function debounce(callback, delay) {
+  var timer;
+  return function() {
+    clearTimeout(timer);
+    timer = setTimeout(callback, delay);
+  };
+}
 
-      if (scroll > 300) {
-        // Apply styles when scroll position is greater than 300
-        navbar.style.boxShadow = "rgba(0, 0, 0, 0.24) 0px 3px 8px";
-        navbar.style.backgroundColor = "#1844A1";
-      } else {
-        // Remove styles when scroll position is less than or equal to 300
-        navbar.style.boxShadow = "none";
-        navbar.style.backgroundColor = "initial"; // or any other default background color
-      }
-    });
+// Function to apply styles on scroll
+function handleScroll() {
+  var scroll = window.scrollY || window.pageYOffset;
+
+  if (scroll > 300) {
+    navbar.style.boxShadow = "rgba(0, 0, 0, 0.24) 0px 3px 8px";
+    navbar.style.backgroundColor = "#1844A1";
+  } else {
+    navbar.style.boxShadow = "none";
+    navbar.style.backgroundColor = "initial";
+  }
+}
+
+// Add a debounced scroll event listener to the window
+window.addEventListener("scroll", debounce(function() {
+  if (!isScrolling) {
+    isScrolling = true;
+    handleScroll();
+    setTimeout(function() {
+      isScrolling = false;
+    }, 100); // Adjust the delay as needed
+  }
+}, 50)); // Adjust the debounce delay as needed
+
   
 
 
